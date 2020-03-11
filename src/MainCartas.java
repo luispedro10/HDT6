c26caa34f53f2a9e60f7a28ebd371faa77f74651
luispedro10 <luis5pedro.2000@gmail.com>
2020-03-10 18:46:38 -0600

    se arreglo, que solo se podia leer el txt en mi computadora

--- src/MainCartas.java
+++ src/MainCartas.java
@@ -4,13 +4,20 @@ import java.util.*;
 public class MainCartas {
 
 	public static void main(String[] args) {
+	Hashmap nuevohash = new Hashmap();
+	leerTxt lector = new leerTxt();
+		
+	
+	lector.abrirArchivo();
+	
+	lector.agregar();
+	
+	
 		
 		
-		leerTxt lector = new leerTxt();
 		
 		
-		lector.abrirArchivo();
-		lector.leer();
+		
 		
 
 	}
--- src/leerTxt.java
+++ src/leerTxt.java
@@ -3,13 +3,16 @@ import java.util.*;
 
 
 
+
 public class leerTxt {
 	
 	private Scanner scan;
 	
+	
+	
 	public void abrirArchivo() {
 		try {
-			scan = new Scanner(new File("C:\\Users\\LUIS PEDRO\\HDT6\\cards_desc.txt"));
+			scan = new Scanner(new File("../cards_desc.txt"));
 		}
 		
 		catch(Exception e) {
@@ -18,22 +21,22 @@ public class leerTxt {
 		}
 	}
 
-	public void  leer () {
+	public void  agregar () {
 		while(scan.hasNextLine()) {
 		String a= (scan.nextLine());
 		String[] parts = a.split("\\|");
-		String part1 = parts[0]; 
+		String part1= parts[0]; 
 		String part2 = parts[1]; 
 		System.out.println(part1);
 		System.out.println(part2);
+		
 		}
 		
-		
-		
-		
-		
+ 
 		
 	}
+	
+	
 	
 			
 			
